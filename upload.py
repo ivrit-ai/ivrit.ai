1ec804bf8d9e50b9153c54a67d2e2a196f6dcdaa
Yair Lifshitz <yair@lifshitz.io>
2023-07-09 23:59:07 +0300

    upload.py: Generate dataset locally.

--- upload.py
+++ upload.py
@@ -6,8 +6,11 @@ import os
 import datasets
 import utils
 
+import mutagen.mp3
 
 def create_dataset(mp3s):
+    total_duration = 0
+
     episodes = []
     sources = []
     uuids = []
@@ -18,10 +21,15 @@ def create_dataset(mp3s):
         source = os.path.basename(os.path.dirname(mp3))
         uuid = f'{source}/{episode}'
 
+        duration = mutagen.mp3.MP3(mp3).info.length
+        total_duration += duration
+
         episodes.append(episode)
         sources.append(source)
         uuids.append(uuid)
-        attrs.append({'license' : 'v1'})
+        attrs.append({'license' : 'v1', 'duration' : duration})
+
+    print(f'Total dataset size is {total_duration:.2f} seconds.')
 
     ds = datasets.Dataset.from_dict({
         'audio' : mp3s,
@@ -41,7 +49,13 @@ def initialize_dataset(args):
 
     ds = create_dataset(mp3s)
 
-    ds.push_to_hub(repo_id=args.dataset, token=args.hf_token)
+    # For now, uploading a dataset of >200GB tends to fail, so we generate a local DB
+    # and upload it using git lfs.
+    #
+    # Yair, July 2023
+
+    #ds.push_to_hub(repo_id=args.dataset, token=args.hf_token)
+    ds.save_to_disk(args.dataset)
 
 if __name__ == '__main__':
     # Define an argument parser
@@ -54,7 +68,7 @@ if __name__ == '__main__':
                         help='Directories to skip. Can be passed multiple times.')
     parser.add_argument('--dataset', type=str, required=True,
                         help='The dataset name to upload.')
-    parser.add_argument('--hf-token', type=str, required=True,
+    parser.add_argument('--hf-token', type=str, required=False,
                         help='The HuggingFace token.')
 
     # Parse the arguments
