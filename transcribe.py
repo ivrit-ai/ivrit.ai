fa0cd63ec43d9e7156081fecbbe8783827f989e0
Yair Lifshitz <yair@lifshitz.io>
2023-07-13 18:24:11 +0300

    transcribe.py: productize.

--- transcribe.py
+++ transcribe.py
@@ -6,7 +6,6 @@ import pathlib
 
 import utils
 
-import openai
 import whisper
 
 model = whisper.load_model('small')
@@ -14,14 +13,21 @@ model = whisper.load_model('small')
 def transcribe(args):
     # Iterate over each root directory
     mp3s = utils.find_files(args.root_dir, args.skip_dir, ['.mp3'])
+    num_files = len(mp3s)
 
     for idx, mp3 in enumerate(mp3s):
-        print(f'Evaluating #{idx+1}: {mp3}')
-        transcribe_single(mp3)
+        print(f'Evaluating #{idx+1}/{num_files}: {mp3}')
+        transcribe_single(mp3, args)
 
-def transcribe_single(mp3):
+def transcribe_single(mp3, args):
     mp3_fn = pathlib.Path(mp3)
-    json_fn = mp3_fn.parent / f'{mp3_fn.stem}.json'
+
+    source = mp3_fn.parent.parent.name
+    episode = mp3_fn.parent.name
+    target_dir = pathlib.Path(args.target_dir) / source / episode
+    pathlib.Path(target_dir).mkdir(parents=True, exist_ok=True)
+
+    json_fn = target_dir / f'{mp3_fn.stem}.json'
 
     try:
         desc = json.load(open(json_fn, 'r'))
@@ -32,7 +38,7 @@ def transcribe_single(mp3):
     
     transcript = model.transcribe(str(mp3_fn), language='he')
 
-    json.dump(transcript, open(json_fn, 'w'))
+    json.dump({'source' : source, 'episode' : episode, 'segment' : mp3_fn.stem, 'text' : transcript['text'], 'transcript_source' : 'asr', 'raw' : transcript}, open(json_fn, 'w'))
 
     print('Done transcribing.')
     
@@ -47,6 +53,8 @@ if __name__ == '__main__':
                         help='Root directory to start search from. Can be passed multiple times.')
     parser.add_argument('--skip-dir', action='append', required=False, default=[],
                         help='Directories to skip. Can be passed multiple times.')
+    parser.add_argument('--target-dir', type=str, required=True,
+                                    help='The directory where splitted audios will be stored.')
 
     # Parse the arguments
     args = parser.parse_args()
