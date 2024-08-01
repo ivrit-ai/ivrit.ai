a41d7ce860361fe5c2671532dbb0b6bee9f62f24
Yoad <yoad@wescover.com>
2024-08-01 23:09:49 +0300

    transcribe - remove partial run state / resume code parts Document server structure mapping reasoning

--- transcribe.py
+++ transcribe.py
@@ -3,26 +3,36 @@
 import argparse
 import json
 import pathlib
-
 import asyncio
-import numpy as np
 from openai import AsyncOpenAI
 from openai.types.audio import Transcription
-import pandas as pd
 
 from utils import utils
 
 
 def map_transcription_response_to_result(transcription: Transcription):
+    """This function maps the output of the transcription server to the expected output format.
+    Specifically, the whisper.cpp implementation does not provide for each segment:
+    - seek
+    - compression_ratio
+    - no_speech_prob
+    And thus, we compensate with filled 0.0 values instead.
+
+    Args:
+        transcription (Transcription): Server Transcription response
+
+    Returns:
+        dict: Segment in expected output format
+    """
     return {
         "segments": [
             {
                 "id": segment["id"],
                 "seek": segment.get("seek") or 0.0,
-                "start": segment["start"] or 0.0,
-                "end": segment["end"] or 0.0,
+                "start": segment["start"],
+                "end": segment["end"],
                 "text": segment["text"],
-                "avg_logprob": segment["avg_logprob"] or 0.0,
+                "avg_logprob": segment["avg_logprob"],
                 "compression_ratio": segment.get("compression_ratio") or 0.0,
                 "no_speech_prob": segment.get("no_speech_prob") or 0.0,
             }
@@ -49,15 +59,11 @@ async def fetch(client: AsyncOpenAI, file_path: str, split_idx: int):
 
 
 async def transcribe_splits(
-    run_state_df: pd.DataFrame,
     desc_filename: pathlib.Path,
     # Batching
     num_splits: int,
-    max_paralle_requests: int,
+    max_parallel_requests: int,
     sleep_between_batches_sec: int,
-    # Partial state
-    checkpoint_every: int,
-    partial_state_store_filename: str,
 ):
     # OAI client for this batch
     client = AsyncOpenAI(
@@ -66,16 +72,10 @@ async def transcribe_splits(
         base_url=args.server_url,
     )
 
-    next_state_store_in = checkpoint_every
-    start_at_split = run_state_df["split_idx"].max() + 1
-    if np.isnan(start_at_split):
-        start_at_split = 0
-    else:
-        print(f"Resuming from split {start_at_split + 1}.")
-
-    for split_base in range(start_at_split, num_splits, max_paralle_requests):
+    results = []
+    for split_base in range(0, num_splits, max_parallel_requests):
         tasks = []
-        for split_idx in range(split_base, min(split_base + max_paralle_requests, num_splits)):
+        for split_idx in range(split_base, min(split_base + max_parallel_requests, num_splits)):
             split_audio_path = desc_filename.parent / f"{split_idx}.mp3"
             if not split_audio_path.exists():
                 raise Exception(f"Unable to find split {split_idx}.")
@@ -86,42 +86,38 @@ async def transcribe_splits(
 
         for r in responses:
             # Per split: id, seek, start, end, text, avg_logprob, compression_ratio, no_speech_prob
-            # Add results to the state dataframe
-            for split_seg_data in r["result"]["segments"]:
-                split_idx = r["split_idx"]
-                split_seg_idx = int(split_seg_data["id"])
-                run_state_df.loc[len(run_state_df)] = [
-                    split_idx,
-                    split_seg_idx,
-                    split_seg_data["seek"],
-                    split_seg_data["start"],
-                    split_seg_data["end"],
-                    split_seg_data["text"],
-                    split_seg_data["avg_logprob"],
-                    split_seg_data["compression_ratio"],
-                    split_seg_data["no_speech_prob"],
-                ]
+            # gather all results
+            segments = []
+            for ith_seg_idx, split_seg_data in enumerate(r["result"]["segments"]):
+                segments.append(
+                    {
+                        "id": int(ith_seg_idx),
+                        "seek": split_seg_data["seek"],
+                        "start": split_seg_data["start"],
+                        "end": split_seg_data["end"],
+                        "avg_logprob": split_seg_data["avg_logprob"],
+                        "compression_ratio": split_seg_data["compression_ratio"],
+                        "no_speech_prob": split_seg_data["no_speech_prob"],
+                        "text": split_seg_data["text"],
+                    }
+                )
+            results.append({"split_idx": int(r["split_idx"]), "segments": segments})
 
         print(f"Done {split_idx + 1}/{num_splits}.")
 
         if sleep_between_batches_sec > 0:
             await asyncio.sleep(sleep_between_batches_sec)
 
-        next_state_store_in -= len(responses)
+    # Properly close the AIO OpenAI client
+    await client.close()
 
-        if 0 == next_state_store_in:
-            next_state_store_in = checkpoint_every
-            run_state_df.to_parquet(partial_state_store_filename)
-
-    if next_state_store_in < checkpoint_every:
-        run_state_df.to_parquet(partial_state_store_filename)
+    return results
 
 
 async def transcribe_audio_source(desc_filename, args):
     source = desc_filename.parent.parent.name
     episode = desc_filename.parent.name
     target_dir = pathlib.Path(args.target_dir) / source / episode
-    partial_state_store_filename = target_dir / f"partial.parquet"
     pathlib.Path(target_dir).mkdir(parents=True, exist_ok=True)
 
     json_fn = target_dir / f"transcripts.json"
@@ -133,26 +129,6 @@ async def transcribe_audio_source(desc_filename, args):
             return
         except:
             pass
-    else:
-        partial_state_store_filename.unlink(missing_ok=True)
-
-    if partial_state_store_filename.exists() and not args.ignore_partial_runs:
-        run_state_df = pd.read_parquet(partial_state_store_filename)
-    else:
-        partial_state_store_filename.unlink(missing_ok=True)
-        run_state_df = pd.DataFrame(
-            {
-                "split_idx": pd.Series(dtype="int"),
-                "segment_idx": pd.Series(dtype="int"),
-                "seek": pd.Series(dtype="float"),
-                "start": pd.Series(dtype="float"),
-                "end": pd.Series(dtype="float"),
-                "text": pd.Series(dtype="str"),
-                "avg_logprob": pd.Series(dtype="float"),
-                "compression_ratio": pd.Series(dtype="float"),
-                "no_speech_prob": pd.Series(dtype="float"),
-            },
-        )
 
     # Open the splits description file
     splits_desc_file = json.load(open(desc_filename))
@@ -160,54 +136,18 @@ async def transcribe_audio_source(desc_filename, args):
     num_splits = len(splits_desc_file["splits"])
     print(f"Total splits: {num_splits}.")
 
-    await transcribe_splits(
-        run_state_df,
+    results = await transcribe_splits(
         desc_filename,
         num_splits,
         args.max_parallel_requests,
         args.sleep_between_batches_sec,
-        args.checkpoint_every,
-        partial_state_store_filename,
     )
-
-    # Convert all state to dump
-
-    results = []
-    for sample_idx in np.sort(run_state_df["split_idx"].unique()):
-        # Get all segments for this sample
-        ith_sample_df = run_state_df[run_state_df["split_idx"] == sample_idx]
-        segments = []
-        for ith_seg_idx in np.sort(ith_sample_df["segment_idx"].unique()):
-            segment_row = ith_sample_df[ith_sample_df["segment_idx"] == ith_seg_idx].iloc[0]
-            segments.append(
-                {
-                    "id": int(ith_seg_idx),
-                    **{
-                        col: float(segment_row[col])
-                        for col in [
-                            "seek",
-                            "start",
-                            "end",
-                            "avg_logprob",
-                            "compression_ratio",
-                            "no_speech_prob",
-                        ]
-                    },
-                    "text": segment_row["text"],
-                }
-            )
-
-        results.append({"split_idx": int(sample_idx), "segments": segments})
 
     json.dump(
         {"source": source, "episode": episode, "transcripts": results},
         open(json_fn, "w"),
         ensure_ascii=False,
     )
-
-    # TODO: Decide if this is a neccesary step or keep using parquet e2e
-    # Clear the state file since it's "done"
-    # partial_state_store_fn.unlink()
 
     print("Done transcribing.")
 
@@ -252,19 +192,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--force-reprocess",
         action="store_true",
-        help="Force retranscription of all files.",
-    )
-    parser.add_argument(
-        "--ignore-partial-runs",
-        action="store_true",
-        help="Do not resume partial runs if found, instead overwrite and start from scratch",
-    )
-    parser.add_argument(
-        "--checkpoint-every",
-        type=int,
-        required=False,
-        default=10,
-        help="Store state every N processed splits.",
+        help="Force re-transcription of all files.",
     )
     parser.add_argument("--server-url", type=str, required=True, help="Transcription server URL.")
     parser.add_argument(
